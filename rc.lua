5ea508f75c2d6971fc070977ea1243c4820365ce
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2024-11-18 18:51:53 +0100

    add use same tag as before restart

--- rc.lua
+++ rc.lua
@@ -88,4 +88,18 @@ end)
 root.buttons(keys.buttons)
 root.keys(keys.globalkeys)
 
+local function load_tag()
+    local f = io.open(os.getenv("HOME") .. "/.awm-tag", "r")
+    if f == nil then
+        return
+    end
+    local tag_name = f:read("*line")
+    f:close()
+    remove_last_tag()
+    local t = awful.tag.find_by_name(nil, tag_name)
+    if t then t:view_only() end
+end
+
+load_tag()
+
 awful.spawn.with_shell(gears.filesystem.get_xdg_config_home() .. "awesome/startup.sh")
--- signals/init.lua
+++ signals/init.lua
@@ -1,3 +1,25 @@
 require("signals.tag")
 require("signals.screen")
 require("signals.client")
+
+function save_current_tag()
+    local t = client.focus and client.focus.first_tag or nil
+    if t == nil then
+        return
+    end
+    local f = assert(io.open(os.getenv("HOME") .. "/.awm-tag", "w"))
+    f:write(t.name, "\n")
+    f:close()
+end
+
+function remove_last_tag()
+    os.remove(os.getenv("HOME") .. "/.awm-tag")
+end
+
+awesome.connect_signal("exit", function (reason_restart)
+    if reason_restart then
+        save_current_tag()
+    else
+        remove_last_tag()
+    end
+end)
