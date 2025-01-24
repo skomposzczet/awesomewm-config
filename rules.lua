0b4a4009c084d121f2c50631c51df4b1d598405d
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2025-01-24 19:25:22 +0100

    update pavucontrol in rules

--- rules.lua
+++ rules.lua
@@ -35,7 +35,7 @@ awful.rules.rules = {
             "Wpa_gui",
             "veromix",
             "xtightvncviewer",
-            "Pavucontrol",
+            "pavucontrol",
         },
 
         -- Note that the name property shown in xprop might be set slightly after creation of the client
