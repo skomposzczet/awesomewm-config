de19d09ceb11c00f4a120b1ebac60d5dc1767126
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2026-05-08 18:02:32 +0200

    improve ght notis

--- signals/ght.lua
+++ signals/ght.lua
@@ -10,15 +10,21 @@ local SET_VAL = 20
 local function send_ght_signal(mode, value, noti)
     local cmd = "xbacklight -get"
 
-    awful.spawn.easy_async_with_shell(cmd, function(level)
+    -- TODO need further improvements
+    awful.spawn.easy_async(cmd, function(level)
         local lvl = tonumber(level)
-        if mode == DELTA then
-            lvl = lvl + value
-        elseif mode == SET then
-            lvl = value
+        if mode then
+            if mode == DELTA then
+                lvl = lvl + value
+            elseif mode == SET then
+                lvl = value
+            else
+                logger.log("light changed with incorrect mode: " .. mode .. "; noti current")
+            end
         else
-            logger.log("light changed with incorrect mode: " .. mode .. "; noti current")
+            logger.log('mode cannot be nil; noti current')
         end
+        lvl = lvl < 0 and 0 or lvl > 100 and 100 or lvl
         local args = {
             level = lvl,
             noti = noti,
@@ -26,8 +32,6 @@ local function send_ght_signal(mode, value, noti)
         awesome.emit_signal("signal::ght", args)
     end)
 end
-
-send_ght_signal()
 
 local function inc()
     send_ght_signal(DELTA, DELTA_VAL, true)
@@ -59,7 +63,7 @@ local function connect_mute()
 end
 
 local function init()
-    send_ght_signal(false)
+    send_ght_signal(nil, nil, false)
     connect_mute()
 end
 
