8d5d20122929869f273429c0fdb26487c2376b0f
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2026-05-08 18:04:33 +0200

    fix clock after sleep

--- widgets/info/clk.lua
+++ widgets/info/clk.lua
@@ -1,6 +1,8 @@
 local wibox = require "wibox"
 local gears = require("gears")
 local theme = require("theme.catppuccin.widgets")
+local lgi = require("lgi")
+local Gio = lgi.require("Gio")
 
 local icon = wibox.widget.textbox()
 icon.font = theme.icon.font .. " 11.5"
@@ -18,6 +20,28 @@ local myclock_t = awful.tooltip {
         return os.date("Today is %A %B %d %Y\nThe time is %T")
     end,
 }
+
+-- https://github.com/awesomeWM/awesome/issues/344
+local function listen_to_signals()
+    local bus = lgi.Gio.bus_get_sync(Gio.BusType.SYSTEM)
+    local sender = "org.freedesktop.login1"
+    local interface = "org.freedesktop.login1.Manager"
+    local object = "/org/freedesktop/login1"
+    local member = "PrepareForSleep"
+    bus:signal_subscribe(sender, interface, member, object, nil, Gio.DBusSignalFlags.NONE,
+    function(bus, sender, object, interface, signal, params)
+        -- "signals are sent right before (with the argument True) and
+        -- after (with the argument False) the system goes down for
+        -- reboot/poweroff, resp. suspend/hibernate."
+        if not params[1] then
+            require("gears.timer").start_new(2, function()
+                myclock_t:force_update()
+            end)
+        end
+    end)
+end
+
+listen_to_signals()
 
 local clk = wibox.widget {
     {
